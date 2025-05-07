78fccb56b2314f0617d5390044de74b1bd9bd4a4
Mansi Rawal <mansirawal99@gmail.com>
2025-05-07 13:33:41 +0530

    added suspense in layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,10 +1,11 @@
 import "./globals.css";
-import LayoutWrapper from '../components/layoutWrapper';
+import { Suspense } from "react";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
       <html lang="en">
         <body className="antialiased bg-gray-50 text-gray-900">
-          <LayoutWrapper>{children}</LayoutWrapper>
+        <Suspense>{children}</Suspense>
         </body>
       </html>
     );
