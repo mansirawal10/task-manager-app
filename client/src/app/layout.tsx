8d62a9e2480128dbf133e5c77cf93cce4a6cb50a
Mansi Rawal <mansirawal99@gmail.com>
2025-05-07 15:40:21 +0530

    trying again

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,12 +1,14 @@
 import "./globals.css";
+import { Suspense } from "react";
 import LayoutWrapper from "../components/layoutWrapper"; // Adjust the path as needed
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
       <html lang="en">
         <body className="antialiased bg-gray-50 text-gray-900">
-        
+        <Suspense fallback={<p>Loading...</p>}>
           <LayoutWrapper>{children}</LayoutWrapper>
+        </Suspense>
 
         </body>
 
